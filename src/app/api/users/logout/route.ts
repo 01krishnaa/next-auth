76eb0d15d33b5d2748a9f5f6e398ad510a8e9c7a
Krishna Singh <krishnasingh781630@gmail.com>
2024-04-02 16:06:19 +0530

    logout route done

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,23 @@
+import { connect } from "@/db/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const response = NextResponse.json({
+      msg: "user logged out",
+      status: 200,
+    });
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json({ error, status: 500 });
+  }
+}
