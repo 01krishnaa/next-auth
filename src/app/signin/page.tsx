4af03ee9714a63bece0021b3be800f4853588890
Krishna Singh <krishnasingh781630@gmail.com>
2024-04-04 01:14:36 +0530

    signin/signup frontend done

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-export default page
+  const [loading, setLoading] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+
+  const router = useRouter();
+
+  const signin = async () => {
+    try {
+      console.log("hi");
+      setLoading(true);
+      const resp = await axios.post("/api/users/signin", user);
+      console.log(resp.data);
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("signin failed");
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+    
+      user.password.length > 0
+    ) {
+      setBtnDisabled(false);
+    } else {
+      setBtnDisabled(true);
+    }
+  }, [user]);
+  return (
+    <div className="p-5 flex flex-col mx-72">
+      <div>{loading ? "Processing" : "Signup Page"}</div>
+      
+      <input
+        className="p-3 text-black"
+        type="text"
+        placeholder="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <br />
+      <input
+        className="p-3 text-black"
+        type="text"
+        placeholder="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <br />
+
+      <button onClick={signin} className="bg-gray-700 p-2">
+        {btnDisabled ? "No signin" : "Signin"}
+      </button>
+      <Link href={"/signup"}>Go to signup page</Link>
+    </div>
+  );
+};
+
+export default page;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,76 @@
-import React from 'react'
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [user, setUser] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
 
-export default page
+  const [loading, setLoading] = useState(false);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+
+  const router = useRouter();
+
+  const signup = async () => {
+    try {
+      console.log("hi");
+      setLoading(true);
+      const resp = await axios.post("/api/users/signup", user);
+      console.log(resp.data);
+      router.push("/signin");
+    } catch (error: any) {
+      console.log("signup failed");
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.username.length > 0 &&
+      user.password.length > 0
+    ) {
+      setBtnDisabled(false);
+    } else {
+      setBtnDisabled(true);
+    }
+  }, [user]);
+  return (
+    <div className="p-5 flex flex-col mx-72">
+      <div>{loading ? "Processing" : "Signup Page"}</div>
+      <input
+        className="p-3 text-black"
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
+      <br />
+      <input
+        className="p-3 text-black"
+        type="text"
+        placeholder="email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <br />
+      <input
+        className="p-3 text-black"
+        type="text"
+        placeholder="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <br />
+
+      <button onClick={signup} className="bg-gray-700 p-2">
+        {btnDisabled ? "No signup" : "Signup"}
+      </button>
+      <Link href={"/signin"}>Go to login page</Link>
+    </div>
+  );
+};
+
+export default page;
